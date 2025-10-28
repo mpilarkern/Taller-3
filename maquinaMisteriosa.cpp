37b4cf59557ca56b3d49ecaa7ca7c4b270df40b5
pilarkern <pilikern04@gmail.com>
2025-10-28 08:41:22 -0300

    primer ejercicio terminado

--- maquinaMisteriosa.cpp
+++ maquinaMisteriosa.cpp
@@ -5,39 +5,51 @@
 
 using namespace std;
 
-int bfs(const vector<vector<int>>& G, int n, int m) {
-    int nodos = G.size();
-    vector<int> pred(nodos);
-    vector<int> orden(nodos);
+int bfs_modificado(const vector<vector<int>>& G, int n, int m) {
+    int cant_nodos = G.size();
+    vector<int> distancias(cant_nodos, -1);
     queue<int> lista;
 
-    int sig = 1;
     int raiz = n;
-    pred[raiz] = 0;
-    orden[raiz] = sig;
     lista.push(raiz);
+    distancias[raiz] = 0;
 
     while (!lista.empty()) {
         int actual = lista.front();
-        for (int vecino: G[actual]) { //no sé qué tan bien está esto
-            if (orden[vecino] == 0) {
-                pred[vecino] = actual;
-                sig = sig + 1;
-                orden[vecino] = sig;
+        for (int vecino: G[actual]) { 
+            if (vecino >= 0 && vecino < cant_nodos && distancias[vecino] == -1) {
+                distancias[vecino] = distancias[actual] + 1;
                 lista.push(vecino);
             }
         }
         lista.pop();
     }
-    //cómo hago para que devuelva la distancia entre n y m?
+    return distancias[m];
 }
 
 vector<vector<int>> misteryMachine(int n, int m) {
-    vector<vector<int>> grafo(2*m + 1);
-    for (int v = 1; v <= 2*m; v++) {
-        grafo[v].push_back(2*v);
-        grafo[v].push_back(v - 1);
+    int cant_max = max(2*m, n);
+    vector<vector<int>> grafo(cant_max + 1);
+    
+    for (int i = 0; i <= cant_max; i++) {
+        if (i - 1 >= 0) {
+            grafo[i].push_back(i - 1);
+        }
+        if (i*2 <= cant_max) {
+            grafo[i].push_back(i*2);
+        }
     }
 
     return grafo;
+}
+
+int main() {
+    int n;
+    int m;
+
+    cin >> n;
+    cin >> m;
+
+    vector<vector<int>> grafo = misteryMachine(n, m);
+    cout << bfs_modificado(grafo, n, m);
 }
