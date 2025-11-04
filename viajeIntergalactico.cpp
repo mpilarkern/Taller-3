9045185cf0554ac2912230c586386385ad2cef19
pilarkern <pilikern04@gmail.com>
2025-11-03 21:22:35 -0300

    fin dijkstra

--- viajeIntergalactico.cpp
+++ viajeIntergalactico.cpp
@@ -10,28 +10,28 @@ long long INF = 1e18;
 
 class grafoPonderado {
 private:
-    vector<vector<pair<int, int>>> N;
+    vector<vector<pair<long long, long long>>> N;
 
 public:
-    void init(int cantNodos, const vector<tuple<int, int, int>>& listaAristas) {
+    void init(long long cantNodos, const vector<tuple<long long, long long, long long>>& listaAristas) {
         N.resize(cantNodos);
 
-        for (tuple<int, int, int> arista: listaAristas) {
-            int v = get<0>(arista);
-            int w = get<1>(arista);
-            int peso = get<2>(arista);
+        for (tuple<long long, long long, long long> arista: listaAristas) {
+            long long v = get<0>(arista);
+            long long w = get<1>(arista);
+            long long peso = get<2>(arista);
 
             N[v].push_back({w, peso});
             N[w].push_back({v, peso});
         }
     }
 
-    int nodos() {
+    long long nodos() {
         return N.size();
     }
 
-    bool tieneArista(int v, int w) {
-        for (pair<int, int> vecino:N[v]) {
+    bool tieneArista(long long v, long long w) {
+        for (pair<long long, long long> vecino:N[v]) {
             if (vecino.first == w) {
                 return true;
             }
@@ -39,13 +39,13 @@ public:
         return false;
     }
 
-    void agregarArista(int v, int w, int peso) {
+    void agregarArista(long long v, long long w, long long peso) {
         N[v].push_back({w, peso});
         N[w].push_back({v, peso});
     }
 
-    long long pesoArista(int v, int w) {
-        for (pair<int, int> vecino: N[v]) {
+    long long pesoArista(long long v, long long w) {
+        for (pair<long long, long long> vecino: N[v]) {
             if (vecino.first == w) {
                 return vecino.second;
             }
@@ -54,33 +54,40 @@ public:
         return INF;
     }
 
-    vector<pair<int, int>> vecindad(int v) {
+    vector<pair<long long, long long>> vecindad(long long v) {
         return N[v];
     }
 };
 
-int dijkstra_modificado(grafoPonderado grafo, int v) {
-    int n = grafo.nodos();
-    priority_queue<pair<long long, int>> candidatos;
-    vector<int> S(n, 0);
+vector<long long> dijkstra_modificado(grafoPonderado& grafo, long long v) {
+    long long n = grafo.nodos();
+    priority_queue<pair<long long, long long>> candidatos;
+    vector<long long> S(n, 0);
     vector<long long> minDist(n, INF);
     
     candidatos.push({0, v});
     minDist[v] = 0;
 
-    while (!candidatos.empty()) {
-        pair<long long, int> candidato = candidatos.top();
+    while (!candidatos.empty()){
+        pair<long long, long long> candidato = candidatos.top();
         long long distancia = -candidato.first;
-        int w = candidato.second;
+        long long w = candidato.second;
         candidatos.pop();
-        
-        for (pair<int, int> arista: grafo.vecindad(w)) { 
-            int u = arista.first;
-            int c = arista.second;
-            if (minDist[u] > distancia + c) {
-                minDist[u] = minDist[w] + c;
+        if (S[w] == 1) continue;
+        S[w] = 1;
+
+        for (pair<long long, long long> vecino: grafo.vecindad(w)) {            
+            if (S[vecino.first] == 0 && vecino.second + minDist[w] < minDist[vecino.first]) {
+                minDist[vecino.first] = vecino.second + minDist[w];
+                long long distancia = -minDist[vecino.first];
+                candidatos.push({distancia, vecino.first});
             }
         }
     }
+    return minDist;
+}
+
+int main() {
+    
     return 0;
 }
